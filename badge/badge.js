2817291541d161eb767b7e442f35b0e07b045493
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-12-27 13:01:46 +0700

    Futile attempt to style the badge

--- badge/badge.js
+++ badge/badge.js
@@ -21,26 +21,70 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 })
 
 function renderRating(payload) {
-  const el = document.createElement('iframe')
+  const link = document.createElement('link')
+  link.href = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+  link.rel = 'stylesheet'
+  document.head.appendChild(link)
+
+  const el = document.createElement('div')
   el.className = 'benarid-chromeextension-badge__root'
 
   el.style.position = 'fixed'
-  el.style.top = '20px'
+  el.style.top = '60px'
   el.style.left = '0'
-  el.style.width = '300px'
+  el.style.width = '350px'
   el.style.background = 'white'
   el.style.zIndex = '9999'
+
+  const controls = document.createElement('div')
+  controls.className = 'benarid-chromeextension-badge__controls'
+
+  const closeButton = document.createElement('div')
+  const expandButton = document.createElement('div')
+
+  controls.appendChild(closeButton)
+  controls.appendChild(expandButton)
+
+  controls.style.position = 'absolute'
+  controls.style.left = '300px'
+  controls.style.width = '30px'
+
+  closeButton.style.height = '30px'
+  closeButton.style.background = '#e74c3c'
+  closeButton.style.color = 'white'
+  closeButton.style.textAlign = 'center'
+  closeButton.style.fontSize = '24px'
+  closeButton.innerHTML = '<i class="fa fa-close"></i>'
+
+  expandButton.style.height = '30px'
+  expandButton.style.background = '#1abc9c'
+  expandButton.style.color = 'white'
+  expandButton.style.textAlign = 'center'
+  expandButton.style.fontSize = '24px'
+  expandButton.innerHTML = '<i class="fa fa-chevron-left"></i>'
+
+  const content = document.createElement('iframe')
+  content.className = 'benarid-chromeextension-badge__content'
+
+  content.frameBorder = '0'
+  content.style.position = 'absolute'
+  content.style.left = '0'
+  content.style.width = '300px'
+  content.style.background = 'white'
+  content.style.boxShadow = '0 0 10px 0 rgba(0,0,0,.3)'
 
   const html =
     ['<ul>']
     .concat(payload.rating.map(({ label, value, count }) =>
       `<li>${label}: ${value} (${count} votes)</li>`
     ))
-    .concat(['</ul>']).join('')
+    .concat(['</ul>'])
+    .join('')
 
-  el.src = `data:text/html;charset=utf-8,${encodeURI(html)}`
+  content.src = `data:text/html;charset=utf-8,${encodeURI(html)}`
 
-  console.log(el.width, el.height)
+  el.appendChild(content)
+  el.appendChild(controls)
 
   document.body.appendChild(el)
 }
