56c5fe8fa1736d5537d44430fe7bfb9a283409d1
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-01-05 09:28:59 +0700

    We have working UI for stats

--- badge/Badge.elm
+++
@@ -1,6 +0,0 @@
-module Badge exposing (..)
-
-import Html exposing (..)
-
-main =
-  text "Badge"
--- badge/badge.js
+++ badge/badge.js
@@ -32,7 +32,7 @@ function renderRating(payload) {
   el.style.position = 'fixed'
   el.style.top = '60px'
   el.style.left = '0'
-  el.style.width = '350px'
+  el.style.maxWidth = '350px'
   el.style.background = 'white'
   el.style.zIndex = '9999'
 
@@ -54,6 +54,7 @@ function renderRating(payload) {
   closeButton.style.color = 'white'
   closeButton.style.textAlign = 'center'
   closeButton.style.fontSize = '24px'
+  closeButton.style.cursor = 'pointer'
   closeButton.innerHTML = '<i class="fa fa-close"></i>'
 
   expandButton.style.height = '30px'
@@ -61,10 +62,30 @@ function renderRating(payload) {
   expandButton.style.color = 'white'
   expandButton.style.textAlign = 'center'
   expandButton.style.fontSize = '24px'
+  expandButton.style.cursor = 'pointer'
   expandButton.innerHTML = '<i class="fa fa-chevron-left"></i>'
 
   const content = document.createElement('iframe')
   content.className = 'benarid-chromeextension-badge__content'
+  const contentId = 'benarid-chromeextension-badge__content'
+  content.setAttribute('id', contentId)
+
+  expandButton.addEventListener('click', () => {
+    if (content.classList.contains('benarid-hidden')) {
+      expandButton.innerHTML = '<i class="fa fa-chevron-left"></i>'
+      content.style.display = 'block'
+      controls.style.left = '300px'
+    } else {
+      expandButton.innerHTML = '<i class="fa fa-chevron-right"></i>'
+      content.style.display = 'none'
+      controls.style.left = '0'
+    }
+    content.classList.toggle('benarid-hidden')
+  })
+
+  closeButton.addEventListener('click', () => {
+    el.parentNode.removeChild(el)
+  })
 
   content.frameBorder = '0'
   content.style.position = 'absolute'
@@ -74,17 +95,68 @@ function renderRating(payload) {
   content.style.boxShadow = '0 0 10px 0 rgba(0,0,0,.3)'
 
   const html =
-    ['<ul>']
-    .concat(payload.rating.map(({ label, value, count }) =>
-      `<li>${label}: ${value} (${count} votes)</li>`
-    ))
-    .concat(['</ul>'])
+    []
+    .concat(payload.rating.map(({ label, sum, count }) => {
+      const percentage = count > 0 ? 100.0 * sum / count : 0
+      const color = getColor(percentage)
+      return `
+        <div class="benarid-chromeextension-badge-content__rating">
+          <div class="benarid-chromeextension-badge-content__header">
+            ${label}:
+            <span class="benarid-count">
+              ${percentage}<span class="benarid-divider">/100 (${count} votes)</span>
+            </span>
+          </div>
+          <div class="benarid-chromeextension-badge-content__value">
+            <div
+              class="benarid-rating-bar benarid-${color}"
+              style="width: ${percentage}%;">
+            </div>
+          </div>
+        </div>
+      `
+    }))
+    .concat(`
+      <div class="benarid-chromeextension-badge-content__rate-button">
+        <button>Nilai artikel ini</button>
+      </div>
+    `)
     .join('')
-
-  content.src = `data:text/html;charset=utf-8,${encodeURI(html)}`
 
   el.appendChild(content)
   el.appendChild(controls)
 
   document.body.appendChild(el)
+
+  fetchTemplateHead()
+    .then(templateHead => {
+      htmlWithHead = templateHead + html
+
+      const iframe = document.getElementById(contentId)
+      iframe.contentWindow.document.open()
+      iframe.contentWindow.document.write(htmlWithHead)
+      iframe.contentWindow.document.close()
+
+      iframe.width  = iframe.contentWindow.document.body.scrollWidth;
+      iframe.height = iframe.contentWindow.document.body.scrollHeight;
+    })
+}
+
+function getColor(percentage) {
+  return percentage < 50 ? 'red' : 'green';
+}
+
+function fetchTemplateHead() {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', chrome.extension.getURL('badge/head.html'), true)
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        resolve(xhr.responseText)
+      }
+    }
+
+    xhr.send()
+  })
 }
