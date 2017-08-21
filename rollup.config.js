7b29894e962bef94234adbe0f8257baf57682448
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-08-21 09:54:21 +0700

    Just put all source in src for simpler structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 export default [{
-  entry: './lib/es6_global/src/popup/popup.js',
+  entry: './lib/es6_global/src/popup.js',
   format: 'iife',
   dest: './static/popup.js',
   moduleName: 'Popup'
 }, {
-  entry: './lib/es6_global/src/background/background.js',
+  entry: './lib/es6_global/src/background.js',
   format: 'iife',
   dest: './static/background.js',
   moduleName: 'Background'
